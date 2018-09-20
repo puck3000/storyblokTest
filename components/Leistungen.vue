06235700145c9bf3c0702b3c1c17e17cce44a080
Daniel Lerch <daniel@lerchpanther.ch>
2018-09-20 12:28:55 +0200

    liste leistungen wie gewünscht => gruusig

--- components/Leistungen.vue
+++ components/Leistungen.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="leistung">
-    <p class="lapcenter lead bmarg2" v-html="$md.render(blok.leistungentext)"></p>
-    <ul v-html="$md.render(blok.liste)"></ul>
+    <div class="lapcenter lead bmarg2" v-html="$md.render(blok.leistungentext)"></div>
+    <div class="liste" v-html="$md.render(blok.liste)"></div>
 </div>
     
 </template>
@@ -14,11 +14,14 @@ props: [
 }
 </script>
 
-<style scoped>
-
-.leistng ul, .leistung p {
-    margin-bottom: 1em;
-}
-
+<style lang="stylus">
+.liste 
+    p 
+        margin-bottom: 2em
+    +lap()
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-row-gap: 4em
+    
 
 </style>
