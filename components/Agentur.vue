89ec2bc6102e7feea88cdd2da7339bba7d67f3f6
Daniel Lerch <daniel@daniellerch.ch>
2019-10-21 15:02:15 +0200

    Update 2019
    - Hopefully fixes double Agency Text on refresh
    - ups Copyright to 2019
    - adds link to Newsletter

--- components/Agentur.vue
+++ components/Agentur.vue
@@ -1,9 +1,9 @@
 <template>
-  <p class="lapcenter lead bmarg2" v-editable="blok" v-html="$md.render(blok.agenturtext)"></p>
+  <article class="lapcenter lead bmarg2" v-editable="blok" v-html="$md.render(blok.agenturtext)"></article>
 </template>
 
 <script>
 export default {
-  props: ['blok']
-}
+  props: ["blok"]
+};
 </script>
--- components/HellaFooter.vue
+++ components/HellaFooter.vue
@@ -1,35 +1,43 @@
 <template>
-   <footer id="mainFooter">
+  <footer id="mainFooter">
     <p>
-      © 2018  HELLASTUDIO.CH
-      <br>
-      PLEASE GET IN TOUCH <a href="tel:+41798308072">+41 (0)79 830 80 72</a>
-      <br>
-      <a href="mailto:hello@hellastudio.ch">HELLO@HELLASTUDIO.CH</a> 
-      <br><br>
-      FOLLOW US ON <a href="https://www.instagram.com/hellastudio.ch/">INSTAGRAM</a>  LIKE US ON <a href="https://www.facebook.com/hellastudio.ch/">FB</a>
-      <br><br>
-      MORE COMING SOON!
+      © 2019 HELLASTUDIO.CH
+      <br />PLEASE GET IN TOUCH
+      <a href="tel:+41798308072">+41 (0)79 830 80 72</a>
+      <br />
+      <a href="mailto:hello@hellastudio.ch">HELLO@HELLASTUDIO.CH</a>
+      <br />
+      <br />FOLLOW US ON
+      <a href="https://www.instagram.com/hellastudio.ch/">INSTAGRAM</a> LIKE US ON
+      <a href="https://www.facebook.com/hellastudio.ch/">FB</a>
+      <br />
+      <br />SUBSCRIBE TO OUR
+      <a target="_blank" href="http://eepurl.com/gG_YGv">NEWSLETTER</a>
     </p>
   </footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="stylus" scoped>
-#mainFooter
-  margin-top: 3em
-  +tab()
-    margin-top: 6em
-  margin-bottom: 3em
-  +tab()
-    margin-bottom: 6em
-  line-height: 1.2em
-  text-align: center
-  font-size: .8em
+#mainFooter {
+  margin-top: 3em;
+
+  +tab() {
+    margin-top: 6em;
+  }
+
+  margin-bottom: 3em;
+
+  +tab() {
+    margin-bottom: 6em;
+  }
+
+  line-height: 1.2em;
+  text-align: center;
+  font-size: 0.8em;
+}
 </style>
 
