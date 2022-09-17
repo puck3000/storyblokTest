346cc22e0d79df185d061191d29164cb16e788bf
Daniel Lerch <daniel.lerch@gmail.com>
2022-09-17 11:52:25 +0200

    better redirect

--- pages/job.vue
+++ pages/job.vue
@@ -1,9 +1,7 @@
 <script>
 export default {
   middleware({ redirect }) {
-    return redirect(
-      "https://hellastudio.ch/Stelleninserat_GraphicDesign_Digital.pdf"
-    );
+    return redirect("@/Stelleninserat_GraphicDesign_Digital.pdf");
   }
 };
 </script>
