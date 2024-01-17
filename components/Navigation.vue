f9fc6bde90f75502add40a3c9caf3b05b42084fb
Daniel Lerch <daniel.lerch@gmail.com>
2024-01-17 11:06:02 +0100

    fix: removes job, adds insta
    * fix: removes job, adds insta

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,16 +1,20 @@
 <template>
-    <nav id="mainNav" :class="{expanded: isExpanded}" class="space">
-        <ul>
-            <li v-for="link in navLinks" :key="link.name" v-on:click="$emit('toggle')">
-                <nuxt-link tag="a" :to="link.url" >{{link.name}}</nuxt-link>
-            </li>
-            <li v-on:click="$emit('toggle')" class="job">
+  <nav id="mainNav" :class="{ expanded: isExpanded }" class="space">
+    <ul>
+      <li
+        v-for="link in navLinks"
+        :key="link.name"
+        v-on:click="$emit('toggle')"
+      >
+        <nuxt-link tag="a" :to="link.url">{{ link.name }}</nuxt-link>
+      </li>
+      <!-- <li v-on:click="$emit('toggle')" class="job">
                 <a href="https://www.hellastudio.ch/Stelleninserat_AccountManagerin_1123.pdf" target="_blank" rel="noopener">
                     Jobs
                 </a>
-            </li>
-        </ul>
-    </nav>
+            </li> -->
+    </ul>
+  </nav>
 </template>
 
 <script>
@@ -21,10 +25,10 @@ export default {
       navLinks: [
         { name: "Agentur", url: "/agentur" },
         { name: "Leistungen", url: "/leistungen" },
-        { name: "Arbeiten", url: "/arbeiten" }
-      ]
+        { name: "Arbeiten", url: "/arbeiten" },
+      ],
     };
-  }
+  },
 };
 </script>
 
@@ -37,7 +41,7 @@ export default {
     position: relative
     top: 0
     left: 0
-    li 
+    li
         text-align: center
         text-transform: uppercase
         height: 3em
@@ -45,15 +49,15 @@ export default {
         border-top: solid 2px $main
         &:last-child
             border-bottom: solid 2px $main
-        a 
+        a
             text-decoration: none
     +lap()
-        .job 
+        .job
             display: none
         ul
-            display: grid 
+            display: grid
             grid-template-columns: repeat(5, 1fr);
-            
+
             li
                 border: none
                 &:first-child
@@ -68,6 +72,4 @@ export default {
         &.expanded
             max-height: 12em
             margin-bottom: 1em;
-        
-
 </style>
